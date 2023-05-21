cb2e0ab05f4087ad85790423507fb235fc14c2d9
carengyl <pavnikol84@gmail.com>
2023-05-22 00:09:30 +0300

    Added utility classes for Server

---
+++ server/src/serverUtil/CollectionOfHumanBeings.java
@@ -0,0 +1,162 @@
+package serverUtil;
+
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import entities.HumanBeing;
+import commonUtil.OutputUtil;
+
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * Collection data structure
+ *
+ * @author carengyl
+ */
+@XStreamAlias("collection")
+public class CollectionOfHumanBeings {
+    /**
+     * File name field
+     */
+    private Path fileName;
+    /**
+     * Initialization date field
+     */
+    private final LocalDate initDate;
+    /**
+     * Hashmap of {@code HumanBeing}
+     */
+    @XStreamImplicit
+    private final HashMap<Long, HumanBeing> humanBeings;
+
+    /**
+     * Constructs new instance with given path
+     *
+     * @param fileName path to file
+     */
+    public CollectionOfHumanBeings(Path fileName) {
+        this.fileName = fileName;
+        humanBeings = new HashMap<>();
+        initDate = LocalDate.now();
+    }
+
+    /**
+     * Constructs new instance with given path and existing hashmap
+     *
+     * @param fileName path to file
+     * @param humanBeings parsed {@code Hashmap<Long, HumanBeing>}
+     */
+    public CollectionOfHumanBeings(Path fileName, HashMap<Long,HumanBeing> humanBeings) {
+        this.fileName = fileName;
+        this.initDate = LocalDate.now();
+        this.humanBeings = humanBeings;
+        HumanBeing.setCurrentId(getMaxID());
+    }
+
+    /**
+     * @return max ID of {@code HumanBeing} from collection
+     */
+    private long getMaxID() {
+        long maxId = 1;
+        for (long key: humanBeings.keySet()) {
+            if (humanBeings.get(key).getId() > maxId) {
+                maxId = humanBeings.get(key).getId();
+            }
+        }
+        return maxId;
+    }
+
+    /**
+     * @return path to file
+     */
+    public Path getFileName() {
+        return fileName;
+    }
+
+    /**
+     * @param fileName new File name
+     */
+    public void setFileName(Path fileName) {
+        this.fileName = fileName;
+    }
+
+    /**
+     * @return Initialization date
+     */
+    public String getInitDate() {
+        return initDate.format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    }
+
+    /**
+     * @param id given id
+     * @return true if given id is in collection
+     */
+    public boolean checkForId(long id) {
+        for (long key: humanBeings.keySet()) {
+            if (humanBeings.get(key).getId().equals(id)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Adds new {@code HumanBeing} to collection
+     *
+     * @param key {@code long} key
+     * @param humanBeing parsed {@code HumanBeing}
+     */
+    public void addByKey(long key, HumanBeing humanBeing) {
+        humanBeings.put(key, humanBeing);
+    }
+
+    /**
+     * @return collection
+     */
+    public HashMap<Long, HumanBeing> getHumanBeings() {
+        return humanBeings;
+    }
+
+    /**
+     * Replaces {@code HumanBeing} by given id with new {@code HumanBeing}
+     *
+     * @param id id of old HumanBeing
+     * @param humanBeing new parsed HumanBeing
+     */
+    public void updateById(long id, HumanBeing humanBeing) {
+        for (long key: humanBeings.keySet()) {
+            if (Objects.equals(humanBeings.get(key).getId(), id)) {
+                humanBeings.replace(key, humanBeing);
+                return;
+            }
+        }
+    }
+
+    /**
+     * Removes all {@code HumanBeing} from collection, if their key is lower than given
+     *
+     * @param greaterKey given key
+     */
+    public void removeLowerKey(long greaterKey) {
+        Set<Long> keysCopy = Set.copyOf(humanBeings.keySet());
+        for(long key: keysCopy) {
+            if (key < greaterKey) {
+                this.removeByKey(key);
+            }
+        }
+    }
+
+    /**
+     * Removes instance from collection by given key
+     *
+     * @param key given key
+     */
+    public void removeByKey(long key) {
+        humanBeings.remove(key);
+        OutputUtil.printSuccessfulMessage("Deleted element by key: " + key);
+    }
+}
---
+++ server/src/serverUtil/StreamUtil.java
@@ -0,0 +1,33 @@
+package serverUtil;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class StreamUtil {
+
+    public ArrayList<String> streamToArrayOfCommands(BufferedInputStream input) throws IOException {
+        ArrayList<String> commands = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(input));
+        String line = br.readLine();
+        while (line != null) {
+            commands.add(line);
+            line = br.readLine();
+        }
+        br.close();
+        return commands;
+    }
+
+
+    public String streamToString(BufferedInputStream inputStream) throws IOException {
+        byte[] contents = new byte[1024];
+        int bytesRead;
+        StringBuilder strFileContents = new StringBuilder();
+        while((bytesRead = inputStream.read(contents)) != -1) {
+            strFileContents.append(new String(contents, 0, bytesRead));
+        }
+        return strFileContents.toString();
+    }
+}
