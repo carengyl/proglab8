60b14653082de3353a7a3024ff11b79f9ec99852
carengyl <pavnikol84@gmail.com>
2023-05-21 23:56:58 +0300

    Added working Serializer and Deserializer

--- common/src/UDPutil/Deserializer.java
+++ common/src/UDPutil/Deserializer.java
@@ -1,7 +1,25 @@
 package UDPutil;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
 public class Deserializer {
-    public static Response deserializeResponse(byte[] bytes){
-        return null;
+    public static Response deserializeResponse(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        Response response = (Response) objectInputStream.readObject();
+        byteArrayInputStream.close();
+        objectInputStream.close();
+        return response;
+    }
+
+    public static Request deserializeRequest(byte[] bytes) throws IOException, ClassNotFoundException {
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        Request request = (Request) objectInputStream.readObject();
+        byteArrayInputStream.close();
+        objectInputStream.close();
+        return request;
     }
 }
--- common/src/UDPutil/Serializer.java
+++ common/src/UDPutil/Serializer.java
@@ -1,13 +1,30 @@
 package UDPutil;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.ByteBuffer;
 
 public class Serializer {
-    public static ByteBuffer serializeRequest(Request request) {
-        return null;
+    public static ByteBuffer serializeRequest(Request request) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(request);
+        objectOutputStream.flush();
+        ByteBuffer bufToSend = ByteBuffer.wrap(byteArrayOutputStream.toByteArray());
+        objectOutputStream.close();
+        byteArrayOutputStream.close();
+        return bufToSend;
     }
 
-    public static ByteBuffer serializeResponse(Response response) {
-        return null;
+    public static ByteBuffer serializeResponse(Response response) throws IOException {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream);
+        objectOutputStream.writeObject(response);
+        objectOutputStream.flush();
+        ByteBuffer bufToSend = ByteBuffer.wrap(byteArrayOutputStream.toByteArray());
+        objectOutputStream.close();
+        byteArrayOutputStream.close();
+        return bufToSend;
     }
 }
