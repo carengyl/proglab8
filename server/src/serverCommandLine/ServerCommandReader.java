56a2b1a122e816ed55df2af1b720703d43f0ff66
carengyl <pavnikol84@gmail.com>
2023-05-22 13:31:08 +0300

    Fixed bug in ServerCommandReader

--- server/src/serverCommandLine/ServerCommandReader.java
+++ server/src/serverCommandLine/ServerCommandReader.java
@@ -8,18 +8,15 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class ServerCommandReader {
-    private Invoker invoker;
-    private Scanner scanner;
+    private final Invoker invoker;
+    private final Scanner scanner;
     public ServerCommandReader(Invoker invoker, Scanner scanner) {
         this.invoker = invoker;
-    }
-
-    private void initCommands() {
-        //invoker.addCommands - - -
+        this.scanner = scanner;
     }
 
     public void readCommandFromConsole() throws NoUserInputException {
-        OutputUtil.printSuccessfulMessage(">");
+        OutputUtil.printSuccessfulMessageOneStrip(">");
         String[] splitString = scanner.nextLine().replaceAll("\s{2,}", " ").strip().split(" ");
         String commandName = splitString[0];
         CommandArgument argument = new CommandArgument(Arrays.copyOfRange(splitString, 1, splitString.length));
